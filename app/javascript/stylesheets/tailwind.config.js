99860c2d54acb8ada8c34fe5ce61a0363280a94b
zaychoer <zaycho_89@yahoo.com>
2020-03-22 23:18:01 +0700

    Scaffold Product, install tailwind

---
+++ app/javascript/stylesheets/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "regal-green": "#282",
+      },
+      boxShadow: {
+        custom: '0.176em 0.176em 0.354em 0px rgba(0, 0, 0, 0.75)'
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
